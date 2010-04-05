3a6fdcc33c262f3e6fa2faf10439e7313dfb1ddc
Nathan Howell <nath@nhowell.net>
2010-04-04 23:58:01 -0700

    post-gnome-switch

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,17 +2,22 @@ import XMonad hiding (Tall)
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import Control.Monad (filterM)
+import Control.OldException
 import Data.Char (ord)
 import Data.List
 import Data.Maybe
 import Data.Ratio
 import System.IO
 import System.Exit
+import DBus
+import DBus.Connection
+import DBus.Message
 
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.CycleWS
 import XMonad.Actions.DynamicWorkspaces
 import XMonad.Actions.FloatKeys
+import XMonad.Actions.RotSlaves
 import XMonad.Actions.Submap
 import XMonad.Actions.UpdatePointer
 import XMonad.Actions.WindowBringer
@@ -22,6 +27,7 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.Place
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
 import qualified XMonad.Layout.BoringWindows as B
@@ -55,13 +61,27 @@ q ~? x = fmap (=~ x) q
 
 
 bg = "#222222"
-fg = "#b67530" --orange
-fn = "-*-liberation.sans-medium-r-*-*-15-*-*-*-*-*-iso8859-*"
-statusBarCmd = "dzen2 -bg '" ++ bg ++ "' -fg '" ++ fg ++ "' -x 0 -y 0 -h 24 -w 1402 -fn '" ++ fn ++ "' -e 'onstart=lower' -ta l"
+fg = "#f3431b"
+fn = "-*-terminal-medium-r-*-*-17-*-*-*-*-*-iso8859-*"
+--fn = "-*-andale mono-medium-r-*-*-17-*-*-*-*-*-*-*"
 
 
-main = do
-  din <- spawnPipe statusBarCmd
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
+                                                getWellKnownName dbus)
+ where
+  tryGetName = do
+    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+    addArgs namereq [String "org.xmonad.Log", Word32 5]
+    sendWithReplyAndBlock dbus namereq 0
+    return ()
+
+
+main :: IO ()
+main = withConnection Session $ \ dbus -> do
+  putStrLn "Getting well-known name."
+  getWellKnownName dbus
+  putStrLn "Got name, starting XMonad."
   --spawn "xcompmgr -nFf -I 0.056 -O 0.06"
   xmonad $ withUrgencyHook NoUrgencyHook
          $ defaultConfig
@@ -70,12 +90,14 @@ main = do
     , normalBorderColor  = "#222222"
     , focusedBorderColor = fg
     , modMask            = mod4Mask
-    , workspaces         = ["0", "comm", "im", "files", "web"]
+    , workspaces         = ["comm", "im", "files", "web"]
     , keys               = myKeys
     , mouseBindings      = myMouse
     , handleEventHook    = ewmhDesktopsEventHook
-    , manageHook         = manageDocks <+> myManageHook
-    , logHook            = myLog din >>
+    , manageHook         = placeHook (inBounds(underMouse (0.5, 0.5)))
+                           <+> manageDocks
+                           <+> myManageHook
+    , logHook            = myLog dbus >>
                            --fadeInactiveLogHook 0x99999999 >>
                            ewmhDesktopsLogHook >>
                            updatePointer (Relative 0.01 0.5) >>
@@ -109,20 +131,32 @@ main = do
              withIM (0.15) (Role "gimp-dock") Full
 
 
-myLog h = withWindowSet $
-  \ws -> dynamicLogWithPP $ defaultPP
-    { ppCurrent         = dzenColor "black" fg . pad
-    , ppUrgent          = dzenColor "black" "#d7c529" . pad . dzenStrip
-    , ppHidden          = dzenColor fg bg
-    , ppHiddenNoWindows = id
-    , ppWsSep           = " "
-    , ppSep             = "  "
-    , ppTitle           = ("^fg(#dddddd)" ++) . dzenEscape
-    , ppOrder           = \(workspaces:layout:title:xs) ->
-                           (myWCount ws:workspaces:title:xs)
-    , ppOutput          = hPutStrLn h
-    }
-    where
+myLog dbus = withWindowSet $ \ws -> do
+        dynamicLogWithPP $ defaultPP
+          { ppCurrent         = pangoStyle "#be462a" "#dfd8c3" "bold"
+          , ppUrgent          = pangoUrgent "#d7c529"
+          , ppHidden          = wrap " " " "
+          , ppVisible         = pangoColor "#663366" . wrap "<" ">"
+          , ppHiddenNoWindows = id
+          , ppWsSep           = ""
+          , ppSep             = " "
+          , ppTitle           = pangoBold "#efefef" . shorten 90
+          , ppOrder           = \(workspaces:layout:title:xs) ->
+                                 (myWCount ws:workspaces:title:xs)
+          , ppOutput   = \ str -> do
+              let str'  = "<span font=\"Liberation Sans Mono\">" ++ str ++ 
+                          "</span>"
+                  str'' = sanitize str'
+              msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" 
+                         "Update"
+              addArgs msg [String str'']
+              -- If the send fails, ignore it.
+              send dbus msg 0 `catchDyn`
+                (\ (DBus.Error _name _msg) ->
+                  return 0)
+              return ()
+            }
+  where
       -- myWCount provides a count of open windows, and
       -- indicates which has focus. e.g. 2/4 means that
       -- the second window of four is focused.
@@ -130,16 +164,38 @@ myLog h = withWindowSet $
                    (\s -> sc ++ (show (length (W.up s) + 1))
                    ++ "/" ++
                    (show (length (W.integrate s))) ++ ec)
-      sc = "^bg(#373737)^fg(" ++ fg ++ ") "
-      ec = " ^fg(" ++ fg ++ ")^bg(" ++ bg ++ ")"
+      sc = "<span foreground=\"#000\" background=\"#dfd8c3\" font_weight=\"bold\"> "
+      ec = " </span>"
 
---       clickable t = do
---         i <- getWsIndex
---         return $ wrap ("^ca(1,xdotool set_desktop "
---                        ++ show (maybe 1 i)
---                        ++ ")")
---                       ("^ca()")
---                       (show t)
+      pangoStyle :: String -> String -> String -> String -> String
+      pangoStyle fg bg fw = wrap left right
+       where
+        left  = "<span foreground=\"" ++ fg ++ "\" background=\"" ++ bg ++ "\" font_weight=\"" ++ fw ++ "\"> "
+        right = " </span>"
+
+      pangoColor :: String -> String -> String
+      pangoColor fg = wrap left right
+       where
+        left  = "<span foreground=\"" ++ fg ++ "\"> "
+        right = " </span>"
+
+      pangoBold :: String -> String -> String
+      pangoBold fg = wrap left right
+       where
+        left  = "<span foreground=\"" ++ fg ++ "\" font_weight=\"bold\"> "
+        right = " </span>"
+
+      pangoUrgent :: String -> String -> String
+      pangoUrgent fg = wrap left right
+       where
+        left  = "<span underline=\"single\" foreground=\"" ++ fg ++ "\" font_weight=\"bold\"> "
+        right = " </span>"
+
+      sanitize :: String -> String
+      sanitize [] = []
+      sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ "; " ++
+                                             sanitize rest
+                        | otherwise        = x : sanitize rest
 
 
 --data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
@@ -156,7 +212,7 @@ myLog h = withWindowSet $
 myPConfig = defaultXPConfig
   { font              = fn
   , bgColor           = bg
-  , fgColor           = fg
+  , fgColor           = "#dfd8c3"
   , height            = 36
   }
 
@@ -267,6 +323,9 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((mod1Mask,                xK_Tab   ), toggleWS )
 
   , ((modMask,                 xK_F12   ), spawn "gnome-screensaver-command --lock")
+
+  , ((modMask .|. controlMask, xK_comma ), rotSlavesUp)
+  , ((modMask .|. controlMask, xK_period), rotSlavesDown)
 
   , ((modMask,                 xK_t     ), B.focusDown)
   , ((modMask,                 xK_n     ), B.focusUp  )
